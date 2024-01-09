a788fd9364054b049c8ff65291a128ec82f63724
nhanbernie <ttnhanbernie@gmail.com>
2024-01-09 07:54:38 +0700

    ToThienNhan: edit Q21

--- QE180206_ToThienNhan/Slot 2/Linked list/Q21/LinkedList.java
+++ QE180206_ToThienNhan/Slot 2/Linked list/Q21/LinkedList.java
@@ -2,28 +2,30 @@ package src;
 
 import java.util.Scanner;
 
-public class LinkedList{
-    public static Node createNode(int value){
+public class LinkedList {
+    public static Node createNode(int value) {
         return new Node(value);
     }
 
-    public static void printList(Node l){
-        Node p = l;
-        while (p != null){
-            System.out.print(p.value + " ");
-            p = p.next;
+    public static void printList(Node head) {
+        Node curent = head;
+        while (curent != null) {
+            System.out.print(curent.value + " ");
+            curent = curent.next;
         }
     }
 
-    public static Node addElement(Node l, int value){
-        Node temp = createNode(value);
-        l.next = temp;
-        return temp;
+    public static Node addElement(Node current, int value) {
+        Node newNode = createNode(value);
+        current.next = newNode;
+        return newNode;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("enter size: ");
         int n = scanner.nextInt();
+        System.out.println("create first node:  ");
         int x = scanner.nextInt();
 
         Node l = createNode(x);
@@ -36,7 +38,6 @@ public class LinkedList{
 
         printList(l);
         scanner.close();
-
 
     }
 }
--- QE180206_ToThienNhan/Slot 2/Linked list/Q21/Node.java
+++ QE180206_ToThienNhan/Slot 2/Linked list/Q21/Node.java
@@ -4,10 +4,11 @@ public class Node {
     int value;
     Node next;
 
-    public Node(){
+    public Node() {
 
     }
-    public Node (int value){
+
+    public Node(int value) {
         this.value = value;
         this.next = null;
     }
