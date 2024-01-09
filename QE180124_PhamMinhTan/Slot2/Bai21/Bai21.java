c8317449caf4cc147d6e9bd31f559fa518fb0acc
tanpmqe180124 <tanpmqe180124@fpt.edu.vn>
2024-01-09 16:29:17 +0700

    DoneBai21

--- QE180124_PhamMinhTan/Slot2/Bai21/Bai21.java
+++ QE180124_PhamMinhTan/Slot2/Bai21/Bai21.java
@@ -1,18 +1,16 @@
-import java.util.LinkedList;
+package Bai21;
+
 import java.util.Scanner;
-// Nhập vào một số nguyên n, tiếp theo là n số nguyên của một dãy số.
 public class Bai21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        LinkedList<Integer> linkedList = new LinkedList<>();
+        LinkedList linkedList = new LinkedList();
         int n = sc.nextInt();
         sc.nextLine();
         for (int i = 1; i <= n; i++) {
             int data = sc.nextInt();
             linkedList.add(data);
         }
-        for (int number : linkedList) {
-            System.out.print(number + " ");
-        }
+        linkedList.showList();
     }
 }
