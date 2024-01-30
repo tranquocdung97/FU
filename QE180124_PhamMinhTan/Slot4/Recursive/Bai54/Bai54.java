0324ec513ca45c88456839086d67f9ebce94beef
tanpmqe180124 <tanpmqe180124@fpt.edu.vn>
2024-01-30 07:11:08 +0700

    Update

---
+++ QE180124_PhamMinhTan/Slot4/Recursive/Bai54/Bai54.java
@@ -0,0 +1,20 @@
+package Bai54;
+
+import java.util.Scanner;
+
+public class Bai54 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(findGCD (a, b));
+        sc.close();
+    }
+
+    private static int findGCD(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return findGCD(b, a % b);
+    }
+}
