bfb450ebc789d9137c13b023c95dbb3450719997
Your Name <you@example.com>
2024-02-04 17:07:05 +0700

    edit code slot 8

--- QE180123_TrinhQuocThang/Slot-8/Ex77/LongestPalindrome.java
+++ QE180123_TrinhQuocThang/Slot-8/Ex77/LongestPalindrome.java
@@ -10,12 +10,6 @@ public class LongestPalindrome {
         y = " " + y;
         int[][] l = new int[m + 1][n + 1];
 
-        for (int i = 0; i <= m; i++)
-            l[i][0] = 0;
-
-        for (int j = 0; j <= n; j++)
-            l[0][j] = 0;
-
         for (int i = 1; i <= m; i++)
             for (int j = 1; j <= n; j++) {
                 if (x.charAt(i) == y.charAt(j))
