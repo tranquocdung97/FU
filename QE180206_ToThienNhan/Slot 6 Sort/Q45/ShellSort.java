2e72fe91a7f4c8ee8c28df6e3737a4bfbac8775a
nhanbernie <ttnhanbernie@gmail.com>
2024-02-21 01:51:38 +0700

    push

---
+++ QE180206_ToThienNhan/Slot 6 Sort/Q45/ShellSort.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ShellSort {
+
+    public static void printArray(int[] a, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void shellSort(int[] a, int n) {
+        int interval, i, j, temp;
+        for (interval = n / 2; interval > 0; interval /= 2) {
+            for (i = interval; i < n; i++) {
+                temp = a[i];
+                for (j = i; j >= interval && a[j - interval] > temp; j -= interval) {
+                    a[j] = a[j - interval];
+                }
+                a[j] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+
+        shellSort(a, n);
+        printArray(a, n);
+    }
+}
